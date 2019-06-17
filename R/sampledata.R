8df9ce578150f1b6d0e165517ba8a5a3dac41aac
Neal Haddaway <neal.haddaway@Neals-MBP.lan>
2019-06-17 08:50:49 +0200

    Sample data inclusion

---
+++ R/sampledata.R
@@ -0,0 +1,25 @@
+#' Systematic review sample data
+#'
+#' Dataset comes from an analysis of systematic reviews (SRs) and systematic maps (SM) published by the
+#' Collaboration for Environmental Evidence (CEE, www.environmentalevidence.org) between May 2012 and
+#' March 2017. Data are based on the empirical study of environmental systematic reviews by Haddaway
+#' and Westgate (2018) https://doi.org/10.1111/cobi.13231.
+#'
+#' @docType data
+#'
+#' @usage data(sampledata)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Haddaway and Westgate (2018) Conservation Biology, 33(2):434-443
+#' (\href{https://doi.org/10.1111/cobi.13231})
+#'
+#' @source \href{https://doi.org/10.1111/cobi.13231}
+#'
+#' @examples
+#' data(sampledata)
+#' str(sampledata)
+
+"sampledata"
