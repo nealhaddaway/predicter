abbe5c8ab2cc47840560dd03537830ac642dc409
Neal Haddaway <neal.haddaway@Neals-MBP.lan>
2019-06-17 09:11:30 +0200

    Update

--- R/predict.time.R
+++ R/predict.time.R
@@ -14,7 +14,7 @@
 #' are provided based on the empirical study of environmental systematic
 #' reviews by Haddaway and Westgate (2018) https://doi.org/10.1111/cobi.13231.
 
-predicTER <- function(planning=20,protocol=5,administration=19,fte=1,db.time=2.25,greylit.time=3.333333,biblcheck.time=1.857601,deduping=1.4,tscreen.time=10.94552,abscreen.time=7.107977,retrieval.time=1.813837,ftscreen.time=11.70102,mdataextr.time=5.997647,ca.time=8.560744,dataextr.time=11.03219,dataprep.time=3.171755,synthesis=15,reportwriting=15,comms=10,meetings=4){
+predict.time <- function(planning=20,protocol=5,administration=19,fte=1,db.time=2.25,greylit.time=3.333333,biblcheck.time=1.857601,deduping=1.4,tscreen.time=10.94552,abscreen.time=7.107977,retrieval.time=1.813837,ftscreen.time=11.70102,mdataextr.time=5.997647,ca.time=8.560744,dataextr.time=11.03219,dataprep.time=3.171755,synthesis=15,reportwriting=15,comms=10,meetings=4){
   time <- ((planning+protocol+db.time+greylit.time+biblcheck.time+deduping+tscreen.time+abscreen.time+retrieval.time+ftscreen.time+mdataextr.time+ca.time+dataextr.time+dataprep.time+synthesis+reportwriting+comms+meetings)/fte)*(1+(administration/100))
   return(time)
 }
